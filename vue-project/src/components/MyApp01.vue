7a6644b9537eba5170cc7e1b88fa14ace2d013d0
dublop <dnlopezavila@gmail.com>
2024-12-14 11:24:52 -0400

    First commit

---
+++ vue-project/src/components/MyApp01.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div>
+        Mi primera App en <span>Vue.js</span>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        color: #35495f;
+        font-size: 1.6rem;
+    }
+    span {
+        color: #41b883;
+        font-weight: 700;
+    }
+</style>
